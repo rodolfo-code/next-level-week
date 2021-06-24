a7a93cecbf457368ca73a2e0146edc37bf949ad0
rodolfo_oliveira <rod.vgo@gmail.com>
2021-06-24 07:45:17 -0300

    pagina de criaçao de sala

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import {Button} from './components/Button'
 import {Home} from './pages/Home'
+import { NewRoom } from './pages/NewRoom'
 
 function App() {
   return (
     <>
-      <Home />
+      {/* <Home /> */}
+      <NewRoom />
     </>
   );
 }
