eda9326a8c11171db9b8f84ac54697194b88fbf0
rodolfo_oliveira <rod.vgo@gmail.com>
2021-06-24 00:13:34 -0300

    iniciando estilizaçao

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import {Button} from './components/Button'
+import {Home} from './pages/Home'
 
 function App() {
   return (
     <>
-    <h1>Hello world</h1>
-    <Button />
+      <Home />
     </>
   );
 }
